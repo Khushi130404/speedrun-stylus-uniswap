cdc7a9f66fdb695e2c79be68f163abf2fcb23eb0
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-08-14 01:22:32 +0530

    Contract Generated

--- packages/nextjs/app/debug/_components/UniswapInterface.tsx
+++ packages/nextjs/app/debug/_components/UniswapInterface.tsx
@@ -189,7 +189,7 @@ export default function UniswapInterface() {
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       const signer = new ethers.Wallet(privateKey, provider);
       const signerAddress = await signer.getAddress();
-      const uniswapContractAddress = "0x40b694c75b4e509aa8cbe9993a37af9546ce36e8";
+      const uniswapContractAddress="0x3e76870eaaed498622a8cf066091a7a0b070b44f";
       const uniswapContract = new ethers.Contract(uniswapContractAddress, IUniswapV2, signer);
       console.log("contract", uniswapContract);
       setContract(uniswapContract);
